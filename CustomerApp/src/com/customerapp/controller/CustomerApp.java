5ee782ab616d37fd2d1588715e45279658d30ed5
VineetRathee <40818026+VineetRathee@users.noreply.github.com>
2021-03-02 19:07:37 +0530

    Update CustomerApp.java

--- CustomerApp/src/com/customerapp/controller/CustomerApp.java
+++ CustomerApp/src/com/customerapp/controller/CustomerApp.java
@@ -11,19 +11,19 @@ public class CustomerApp {
 	public static void main(String[] args) {
 		CustomerService customerService = new CustomerServiceImpl();
 		 
-	    Date date1 = new Date();
+	    Date tempDate = new Date();
 
-		Customer customer1 = new Customer("Ravi", "Hisar", "3984379854",date1);
-		Customer customer2 = new Customer("Umesh", "Delhi", "2832344376",date1);
+		Customer customer1 = new Customer("vineet", "rohtak", "44444444",tempDate);
+		Customer customer2 = new Customer("keshav", "dadri", "283566545623",tempDate);
 
 		System.out.println("Add customer by id");
 		customerService.addCustomer(customer1);
 		customerService.addCustomer(customer2);
 
 		System.out.println("Get customer by id ");
-         System.out.println(customerService.getCustomerById(1));
+            System.out.println(customerService.getCustomerById(1));
 		System.out.println(customerService.getCustomerById(2));
-		System.out.println(customerService.getCustomerById(10));
+		System.out.println(customerService.getCustomerById(25));
 
 	}
 }
